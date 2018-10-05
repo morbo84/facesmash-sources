1fcbe92517d73603d8fe4b6cbab9d09fffc2caf5
Michele Caini <michele.caini@gmail.com>
2018-10-05 14:05:24 +0200

    revert flipping video frames

--- src/system/recording_system.cpp
+++ src/system/recording_system.cpp
@@ -93,7 +93,7 @@ void RecordingSystem::update(GameRenderer &renderer, delta_type delta, std::func
             next();
 
             renderer.target(*recording);
-            SDL_RenderCopyEx(renderer, *logical, nullptr, nullptr, 0., nullptr, SDL_FLIP_HORIZONTAL);
+            SDL_RenderCopy(renderer, *logical, nullptr, nullptr);
 
             renderer.target();
             SDL_RenderCopy(renderer, *logical, nullptr, nullptr);
